8671c2454f6de8aff5eed8078198fc3f5fe1e91d
Felipe Barbosa da Silva <felipe.bsilva@a.ficr.edu.br>
2021-11-11 22:07:19 -0300

    alterações

--- js/global.js
+++ js/global.js
@@ -1,6 +1,10 @@
 const cardnv = document.getElementById('cardnv')
 const btnnv = document.getElementById('btnnv')
 const lost = document.getElementById('lost')
+const formnvlista = document.getElementById('nvlista')
+
+const listas = []
+
 
 btnnv.addEventListener('click', function() {
     cardnv.style.display = 'block'
@@ -8,3 +12,19 @@ btnnv.addEventListener('click', function() {
 lost.addEventListener('click', function() {
     cardnv.style.display = 'none'
 })
+formnvlista.addEventListener('submit', function(event) {
+    event.preventDefault()
+    const inputTitulo = document.querySelector('input[name="titulolista"]')
+    if (inputTitulo.value !== ''){
+        const nvlista = {
+            id: listas.length + 1,
+            titulo: inputTitulo.value
+        }
+        inputTitulo.value = ''
+        listas.push(nvlista)
+        console.log(listas)
+        window.localStorage.listas = JSON.stringify(listas)
+    }
+    console.log(listas)
+})
+
